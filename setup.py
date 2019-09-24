33cb2e387fc73a10db415d4028a33d5f0786d6cb
dark-ai <ac990731@gmail.com>
2019-09-24 19:23:51 +0800

    change setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='pypytorch',
-    version='0.0.1',
+    version='0.0.1a',
     description='PyPyTorch is a simple deep learning framework',
     author='dark-ai',
     author_email='ac990731@gmail.com',
